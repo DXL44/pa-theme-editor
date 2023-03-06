d38f2dbe572425433384897d6933888d217acf2b
DestroyerXL44 <28364459+DXL44@users.noreply.github.com>
2023-03-06 10:50:43 -0500

    CreateInputs loads theme storage colors !!!

--- js/create-inputs.js
+++ js/create-inputs.js
@@ -1,26 +1,82 @@
+//Before we do anything, we need a way to keep track of the theme
+const themeStorage = {
+    "id": 971885,
+    "name": "machine",
+    "gui": "94d8db",
+    "bg": "212121",
+    "players": [
+      "e57373",
+      "64b5f6",
+      "81c784",
+      "ffb74d"
+    ],
+    "objs": [
+      "c0ace1",
+      "f17bb8",
+      "2f426d",
+      "1b1b1c",
+      "efebef",
+      "efebef",
+      "efebef",
+      "efebef",
+      "efebef"
+    ],
+    "bgs": [
+      "94d8db",
+      "e57373",
+      "d8ffe0",
+      "f1ffc4",
+      "fefec0",
+      "f2c7b7",
+      "f2c7b7",
+      "f2c7b7",
+      "f2c7b7"
+    ]
+  }
 //SO. You have 24 lines of 24 <input>s in your html code. What do you do? 
 //Answer: Try and probably fail to automate it. Let's go!!!!!!!
-function CreateInputs(){
-    for (let i = 1; i < 25; i++) {
+function CreateInputs(from,to,group,sort){
+    console.log("Creating inputs in group " + group + "...")
+    for (let i = 0; i < ((to-from)+1); i++) {
         // its time for the input!!!
         var input = document.createElement("input");
-        // <input class="ThemeColor" type="color" onchange="UpdateColors()" id="col[i]picker" value="[default value]">
+        // <input class="ThemeColor" type="color" onchange="Refresh([group])" id="col1picker" value="#94D8DB">
         input.className = "ThemeColor";
         input.type = "color";
-        input.id = "col" + i + "picker";
-        input.value = "#000000"
-        input.setAttribute("onchange", "UpdateColors()");
+        input.id = group + i;
+        //switcheroo for the value!!
+        switch (group){
+            case "gui":
+                basecolor =  themeStorage.gui[i]
+                break;
+            case "bg":
+                basecolor = themeStorage.bg[i]
+                break;
+            case "players":
+                basecolor =  themeStorage.players[i]
+                break;
+            case "objs":
+                basecolor = themeStorage.objs[i] 
+                break;
+            case "bgs":
+                basecolor = themeStorage.bgs[i]
+                break;
+        }
+        input.setAttribute("value", "#" + basecolor);
+        input.setAttribute("onchange", 'RefreshColor("' + group + '",' + i +')'); //" + group + "," + i + " // RefreshColor(players,3)
 
         //and now for the span (!!)
         var span = document.createElement("span");
         //<span id="col[x]text">C0ACE1</span>
-        span.id = "col" + i + "text";
-        span.innerHTML = "000000"
+        span.id = group + i + "text";
+        span.innerHTML = basecolor
         //now we break it down 
         var br = document.createElement("br");
         // now...create the inputs. lets do this thing
         document.body.appendChild(input);
         document.body.appendChild(span);
         document.body.appendChild(br);
+        console.log(i + " - " + basecolor)
     }
+    console.log("All done!")
 }
--- js/theme-editor.js
+++ js/theme-editor.js
@@ -1,42 +1,10 @@
 	/* all of the code here is probably really bad i'm sorry lmao */
 // ALRIGHT NERDS. its time to put together the theme file
-const themeStorage = {
-    "id": 971885,
-    "name": "machine",
-    "gui": "94d8db",
-    "bg": "212121",
-    "players": [
-      "e57373",
-      "64b5f6",
-      "81c784",
-      "ffb74d"
-    ],
-    "objs": [
-      "c0ace1",
-      "f17bb8",
-      "2f426d",
-      "1b1b1c",
-      "efebef",
-      "efebef",
-      "efebef",
-      "efebef",
-      "efebef"
-    ],
-    "bgs": [
-      "94d8db",
-      "e57373",
-      "d8ffe0",
-      "f1ffc4",
-      "fefec0",
-      "f2c7b7",
-      "f2c7b7",
-      "f2c7b7",
-      "f2c7b7"
-    ]
-  }
+
 //Its time to update the colors but be WAY COOLER
 //COLOR IDS will be in this format: [group][number]. 
 //COLOR GROUPS: gui, bg, players, objs, bgs (aka the groups in the uh... uh the )
+
 function RefreshColor(group,num){
     var col = document.getElementById(group + num).value;
     //Pull the ol' switcheroo based on what the color's group is

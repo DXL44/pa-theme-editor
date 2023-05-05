d8f3db2f4845a652c7511f7ca2b788e50a0f0fb5
DestroyerXL44 <28364459+DXL44@users.noreply.github.com>
2023-05-05 19:08:54 -0400

    Inputs for ID, display name, file name

--- js/theme-editor.js
+++ js/theme-editor.js
@@ -33,19 +33,34 @@ function RefreshColor(group,num){
 
 //make the theme! woohoo!    
 function CreateTheme(){
+    //Check what all of these are
+    InpThemeDisplay = document.getElementById("InpThemeDisplay").value
+    InpThemeID = document.getElementById("InpThemeID").value
         /*Name the theme*/
         let text;
-          let themename = prompt("Name your theme:",);
+          let themename = document.getElementById("InpThemeName").value; 
           if (themename == null || themename == "") {
             console.log("Invalid theme name - cancelled");
+            alert("You need a proper theme name!")
             return
         }
-        themeStorage.name = themename
+        // chcek if theme display exists. if it does, use it. if it doesn't, don.t
+        if (InpThemeDisplay == null || InpThemeDisplay == ""){
+            themeStorage.name = themename
+        } else {
+            themeStorage.name = InpThemeDisplay
+        }
         /*In Project Arrhythmia, theme IDS are actually just random numbers with no significance whatsoever*/
-        themeStorage.id = Math.round(Math.random()*1000000);
+        //we need to check if it's a number we can use (is an actual number, is whole)
+        if (InpThemeID == null || InpThemeID == "" || isNaN(InpThemeID) == true || (InpThemeID - Math.floor(InpThemeID)) !== 0){
+            console.log("No valid theme ID, using random");
+            themeStorage.id = Math.round(Math.random()*1000000);
+        } else {
+            themeStorage.id = InpThemeID;
+        }
         var themeJSON = JSON.stringify(themeStorage);
         /*Now download it*/
-        download(themename + ".lst", themeJSON)
+        download(themename + ".lst", themeJSON);
     } 
 function download(filename, text){
         var element = document.createElement('a');
@@ -89,7 +104,24 @@ function loadTheme() {
         for (let i = 0; i < 9; i++) {
             themeStorage.bgs[i] = inputTheme.bgs[i]
         }
+        //set values of theme info inputs
+        document.getElementById("InpThemeName").setAttribute("value", inputTheme.name);
+        document.getElementById("InpThemeDisplay").setAttribute("value", inputTheme.name);
+        document.getElementById("InpThemeID").setAttribute("value", inputTheme.id)
         AssembleInputs()
     }
 }
-//load the theme
+//hide background
+
+function hideBackground() {
+    // make the check the check
+    bgcheck = document.getElementById("hideBGs")
+    if (bgcheck.checked == true) {
+    for (let i = 0; i < 9; i++){ // all 9 theme colors
+    themeStorage.bgs[i] = themeStorage.bg // set them to the theme color
+    RefreshColor("bgs",i) // then reload those colors
+    }
+    } else {
+    console.log("nah nvm")
+    }
+}
